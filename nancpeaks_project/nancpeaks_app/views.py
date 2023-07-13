f59e3e1ce7e3e98d369ebe4180a3aad5c5fb0011
ionlabs <33338812+hariharancse11@users.noreply.github.com>
2023-07-13 13:28:12 +0530

    change

--- nancpeaks_project/nancpeaks_app/views.py
+++ nancpeaks_project/nancpeaks_app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render,HttpResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from gtts import gTTS
+from io import BytesIO
+from django.http import HttpResponse
 
 # Create your views here
 @api_view(['POST'])
 def to_speech(request):
-    from gtts import gTTS
-    import os
-
     text = request.data.get('text')
     language = request.data.get('language')
     print(text, language)
@@ -15,21 +15,13 @@ def to_speech(request):
     # Create a gTTS object
     tts = gTTS(text, lang=language)
 
-    # Save the audio as a temporary file
-    temp_file = "audio.mp3"
-    tts.save(temp_file)
-
-    audio_file_path = temp_file  # Replace with the actual audio file path
-
-    print("Text converted to speech")
-
-    with open(audio_file_path, 'rb') as f:
-        audio_data = f.read()
+    # Generate the audio data in memory
+    audio_data = BytesIO()
+    tts.save(audio_data)
+    audio_data.seek(0)
 
     response = HttpResponse(audio_data, content_type='audio/mpeg')
     response['Content-Disposition'] = 'attachment; filename="audio.mp3"'
 
-    # Delete the temporary audio file
-    os.remove(temp_file)
-
     return response
+
