b543a435c6420dabe9eeaf13862be6284ef443a1
ionlabs <33338812+hariharancse11@users.noreply.github.com>
2023-07-13 13:15:32 +0530

    yes

--- nancpeaks_project/nancpeaks_app/views.py
+++ nancpeaks_project/nancpeaks_app/views.py
@@ -6,11 +6,11 @@ from rest_framework.decorators import api_view
 @api_view(['POST'])
 def to_speech(request):
     from gtts import gTTS
-    from pydub import AudioSegment
     import os
 
-    text,language = request.data.get('text'), request.data.get('language')
-    print(text,language)
+    text = request.data.get('text')
+    language = request.data.get('language')
+    print(text, language)
 
     # Create a gTTS object
     tts = gTTS(text, lang=language)
@@ -18,21 +18,6 @@ def to_speech(request):
     # Save the audio as a temporary file
     temp_file = "audio.mp3"
     tts.save(temp_file)
-
-
-    # # Load the audio file using pydub
-    # audio = AudioSegment.from_file(temp_file, format="mp3")
-    # print(temp_file)
-    #
-    # # Adjust the speed by changing the framerate
-    # speed_multiplier = speed  # Increase by 50%
-    # adjusted_audio = audio._spawn(audio.raw_data, overrides={
-    #     "frame_rate": int(audio.frame_rate * speed_multiplier)
-    # })
-    #
-    # # Export the adjusted audio
-    # output_file = "audio.mp3"
-    # adjusted_audio.export(output_file, format="mp3")
 
     audio_file_path = temp_file  # Replace with the actual audio file path
 
@@ -44,10 +29,7 @@ def to_speech(request):
     response = HttpResponse(audio_data, content_type='audio/mpeg')
     response['Content-Disposition'] = 'attachment; filename="audio.mp3"'
 
-    # Save the audio file in the local project folder
-    project_folder = os.path.dirname(os.path.abspath(__file__))
-    local_file_path = os.path.join(project_folder, 'audio.mp3')
-    with open(local_file_path, 'wb') as f:
-        f.write(audio_data)
+    # Delete the temporary audio file
+    os.remove(temp_file)
 
     return response
