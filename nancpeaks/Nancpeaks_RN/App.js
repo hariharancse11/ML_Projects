f08a79b4c3c6117ba3900c89371fbcccda42b1ec
Hariharan <33338812+hariharancse11@users.noreply.github.com>
2023-07-18 18:40:38 +0530

    YES

--- nancpeaks/Nancpeaks_RN/App.js
+++ nancpeaks/Nancpeaks_RN/App.js
@@ -1,64 +1,59 @@
-import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Button, View, TextInput, Platform } from 'react-native';
+import { StyleSheet, Button, View, Platform } from 'react-native';
+import * as FileSystem from 'expo-file-system';
+import { shareAsync } from 'expo-sharing';
+import {useState} from 'react';
 
 export default function App() {
-  const [text, setText] = useState('');
-  const [language, setLanguage] = useState('');
-  const [speed, setSpeed] = useState('');
+  const [text, setText] = useState('You made it!');
+  const [language, setLanguage] = useState('en');
+  const [speed, setSpeed] = useState(1);
+  
+  const downloadFromAPI = async () => {
 
-  const postToAPI = async () => {
-    const url = 'http://35.173.186.103:8000/';
-
+    const filename = "MissCoding.pdf";
+    const localhost = Platform.OS === "android" ? "10.0.2.2" : "127.0.0.1";
     const requestBody = {
       text,
       language,
-      speed,
+      speed
     };
-
-    try {
-      const response = await fetch(url, {
+    const result = await FileSystem.downloadAsync(
+      `http://35.173.186.103:8000/`,
+      FileSystem.documentDirectory + filename,
+      {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(requestBody),
-      });
-
-      if (response.ok) {
-        // Request succeeded
-        const result = await response.json();
-        console.log(result);
-      } else {
-        // Request failed
-        console.log('Request failed:', response.status);
+        body: JSON.stringify(requestBody)
       }
-    } catch (error) {
-      console.log('Error:', error.message);
+    );
+    console.log(result);
+    save(result.uri, filename, result.headers["Content-Type"]);
+  };
+
+  const save = async (uri, filename, mimetype) => {
+    if (Platform.OS === "android") {
+      const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
+      if (permissions.granted) {
+        const base64 = await FileSystem.readAsStringAsync(uri, { encoding: FileSystem.EncodingType.Base64 });
+        await FileSystem.StorageAccessFramework.createFileAsync(permissions.directoryUri, filename, mimetype)
+          .then(async (uri) => {
+            await FileSystem.writeAsStringAsync(uri, base64, { encoding: FileSystem.EncodingType.Base64 });
+          })
+          .catch(e => console.log(e));
+      } else {
+        shareAsync(uri);
+      }
+    } else {
+      shareAsync(uri);
     }
   };
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter text"
-        value={text}
-        onChangeText={setText}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Enter language"
-        value={language}
-        onChangeText={setLanguage}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Enter speed"
-        value={speed}
-        onChangeText={setSpeed}
-      />
-      <Button title="POST to API" onPress={postToAPI} />
+        <Button title="Download From API" onPress={downloadFromAPI} />
       <StatusBar style="auto" />
     </View>
   );
@@ -70,13 +65,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  input: {
-    width: '80%',
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 10,
-    paddingHorizontal: 10,
   },
 });
--- nancpeaks/nancpeaks/nancpeaks_app/views.py
+++ nancpeaks/nancpeaks/nancpeaks_app/views.py
@@ -13,7 +13,7 @@ from django.core.files import File
 
 
 @csrf_exempt
-@api_view(['POST'])
+@api_view(['GET'])
 def to_speech(request):
     text, language, speed = request.data.get('text'),request.data.get('language'),request.data.get('speed')
     # Example usage
